16f2a80d478e942af95ee05efe4d371f24f4f46b
Torehpies <salupadoseiffer@gmail.com>
2025-09-27 11:59:08 +0800

    AC-5 Setup Firebase auth and update configs

--- lib/controllers/register_controller.dart
+++ lib/controllers/register_controller.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:logging/logging.dart';
 import '../models/appuser.dart';
 
 class RegisterController {
