c1148185ee6a98f333bdbfdc9d446d4aefc03cea
Erricka <errickajoyalera14@gmail.com>
2025-09-27 16:32:27 +0800

    act log, home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import '../components/activity-logs.dart';
-=======
-
 import '../components/activity-logs.dart';
-
 import '../components/activity-logs.dart'; 
 
->>>>>>> 7334e288c73827a4b566a3c9702ec39df37ce557
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-<<<<<<< HEAD
-=======
 
->>>>>>> 7334e288c73827a4b566a3c9702ec39df37ce557
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
@@ -82,10 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-<<<<<<< HEAD
-=======
 
->>>>>>> 7334e288c73827a4b566a3c9702ec39df37ce557
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -98,24 +88,13 @@ class _HomeScreenState extends State<HomeScreen> {
             constraints: const BoxConstraints(maxWidth: 500),
             padding: const EdgeInsets.all(24),
             child: Column(
-<<<<<<< HEAD
-=======
 
->>>>>>> 7334e288c73827a4b566a3c9702ec39df37ce557
-              children: [
-                const Spacer(),
-                // Pass logs to card
-                CustomCard(title: "Activity Logs", logs: _wasteLogs),
-                const SizedBox(height: 20),
-<<<<<<< HEAD
-=======
-  children: const [
-                Spacer(), // pushes card to the bottom
-                ActivityLogs(title: "Activity Logs"),
-                SizedBox(height: 20),
-
->>>>>>> 7334e288c73827a4b566a3c9702ec39df37ce557
-              ],
+  children: [
+    const Spacer(),
+    // Pass logs to card
+    CustomCard(title: "Activity Logs", logs: _wasteLogs),
+    const SizedBox(height: 20),
+  ],
             ),
           ),
         ),
