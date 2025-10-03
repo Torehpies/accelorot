d816f375a404ff5dd2cb7a5a0a3860eec3b7d8bd
Shinzo <131604296+Shinzho@users.noreply.github.com>
2025-10-03 23:31:34 +0800

    AC-71 completed admin nav bar
    new admin side

--- lib/screens/admin/admin_profile_screen.dart
+++ lib/screens/admin/admin_profile_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import '../login_screen.dart'; // Import your login screen (adjust path if needed)
 
 class AdminProfileScreen extends StatelessWidget {
   const AdminProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Admin Profile"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            // Navigate back to Login Screen
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginScreen()),
+            );
+          },
+        ),
+      ),
+      body: const Center(
         child: Text(
           "Admin Profile Screen",
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
