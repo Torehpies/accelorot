30d00bb69af6b6d1dfac8e21ca403359e6ac89c7
Torehpies <salupadoseiffer@gmail.com>
2025-10-08 19:11:58 +0800

    AC-102 Updated main navigation

--- lib/screens/main_navigation.dart
+++ lib/screens/main_navigation.dart
@@ -30,7 +30,6 @@ class _MainNavigationState extends State<MainNavigation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //body: _screens[_selectedIndex],
 			body: IndexedStack(
 				index: _selectedIndex,
 				children: _screens,
