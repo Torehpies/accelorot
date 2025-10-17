d3b52fcd87cedaac4bb0ed52a2f60a296ecaf6e5
Torehpies <salupadoseiffer@gmail.com>
2025-10-18 01:47:29 +0800

    AC-102 addded app user model

---
+++ lib/data/models/app_user_model.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AppUser {
+  const AppUser({
+    required this.uid,
+    this.email,
+    this.emailVerified = false,
+    this.displayName,
+  });
+
+  final String uid;
+  final String? email;
+  final bool emailVerified;
+  final String? displayName;
+
+  static AppUser? fromUser(User? user) {
+    if (user == null) {
+      return null;
+    }
+    return AppUser(
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      emailVerified: user.emailVerified,
+    );
+  }
+}
