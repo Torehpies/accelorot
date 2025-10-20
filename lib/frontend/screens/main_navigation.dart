c631dd00a5478cf253c288fcefbe85524c7450fa
Shinzo <131604296+Shinzho@users.noreply.github.com>
2025-10-20 21:59:06 +0800

    AC-175 TEST

--- lib/frontend/screens/main_navigation.dart
+++ lib/frontend/screens/main_navigation.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
+import '../operator/dashboard/home_screen.dart';
 import 'package:flutter_application_1/frontend/operator/activity_logs/widgets/activity_logs_navigator.dart';
 import 'statistics_screen.dart';
 import 'profile_screen.dart';
