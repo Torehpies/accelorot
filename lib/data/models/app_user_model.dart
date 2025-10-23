6f6940e208b334ae7c3a4135949a66a0b8b04e8f
Torehpies <salupadoseiffer@gmail.com>
2025-10-23 18:06:11 +0800

    AC-102 Updated appusermodel

--- lib/data/models/app_user_model.dart
+++ lib/data/models/app_user_model.dart
@@ -6,12 +6,17 @@ class AppUser {
     this.email,
     this.emailVerified = false,
     this.displayName,
+    required this.role,
+    this.fullName,
   });
 
   final String uid;
   final String? email;
   final bool emailVerified;
   final String? displayName;
+
+  final String role;
+  final String? fullName;
 
   static AppUser? fromUser(User? user) {
     if (user == null) {
@@ -22,6 +27,27 @@ class AppUser {
       email: user.email,
       displayName: user.displayName,
       emailVerified: user.emailVerified,
+      role: 'operator',
+      fullName: user.displayName,
     );
   }
+
+  factory AppUser.fromFirestore(
+    User firebaseUser,
+    Map<String, dynamic> firestoreData,
+  ) {
+    return AppUser(
+      uid: firebaseUser.uid,
+      email: firebaseUser.email,
+      displayName: firebaseUser.displayName,
+      emailVerified: firebaseUser.emailVerified,
+      role: firestoreData['role'] as String? ?? 'operator',
+      fullName: firestoreData['fullName'] as String?,
+    );
+  }
+
+	bool get isAdmin => role == 'admin';
+	bool get isOperator => role == 'operator';
+	bool get isStandardUser => role == 'operator';
+
 }
