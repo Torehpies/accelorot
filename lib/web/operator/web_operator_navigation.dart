372a106bae9a9441c8d3ae7e9b7cccd9eaa0006f
rena <renagiray17@gmail.com>
2025-10-29 12:10:35 +0800

    conflict

--- lib/web/operator/web_operator_navigation.dart
+++ lib/web/operator/web_operator_navigation.dart
@@ -35,7 +35,7 @@ class _WebOperatorNavigationState extends State<WebOperatorNavigation> {
     super.initState();
     _screens = [
       const WebHomeScreen(),
-      const WebActivityLogsScreen(),
+      const WebActivityLogsScreen(shouldRefresh: false),
       const WebStatisticsScreen(),
       const WebOperatorMachineScreen(),
       const WebProfileScreen(),
