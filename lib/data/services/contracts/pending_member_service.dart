faa5fc27c26aeeddbb0f0ffcbbf6e425d78da152
Torehpies <salupadoseiffer@gmail.com>
2025-11-22 23:55:49 +0800

    AC-249 Modified pending member service for add and delete functions

--- lib/data/services/contracts/pending_member_service.dart
+++ lib/data/services/contracts/pending_member_service.dart
@@ -1,10 +1,19 @@
 import 'package:flutter_application_1/data/services/contracts/pagination_result.dart';
 
 abstract class PendingMemberService {
+	Future<String> addPendingMember({
+		required String teamId,
+		required String memberId,
+		required String memberEmail
+	});
 	Future<Map<String, dynamic>?> fetchRawPendingMemberData(String id);
 	Future<PaginationResult<Map<String, dynamic>>> fetchRawPendingMembers({
 		required String teamId,
 		required int limit,
 		String? startCursor,
+	});
+	Future<void> deletePendingMember({
+		required String teamId,
+		required String memberId,
 	});
 }
