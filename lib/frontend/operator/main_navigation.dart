89785fcdcd4a523bead9753f0af66737ad7ec59c
Shinzo <131604296+Shinzho@users.noreply.github.com>
2025-11-29 10:29:15 +0800

    reroute

--- lib/frontend/operator/main_navigation.dart
+++ lib/frontend/operator/main_navigation.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/frontend/operator/statistics/statistics_screen.dart';
 import 'dashboard/home_screen.dart';
-import 'package:flutter_application_1/frontend/operator/activity_logs/widgets/activity_logs_navigator.dart';
+import 'package:flutter_application_1/ui/activity_logs/view/activity_logs_navigator.dart';
 import 'profile/profile_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'machine_management/operator_machine/operator_machine_screen.dart';
@@ -66,7 +66,7 @@ class _MainNavigationState extends State<MainNavigation> {
       ),
       StatisticsScreen(
         focusedMachineId: widget.focusedMachine?.machineId,
-        focusedMachine: widget.focusedMachine, // ⭐ Pass the full machine object
+        focusedMachine: widget.focusedMachine,
       ),
       const OperatorMachineScreen(),
       const ProfileScreen(),
