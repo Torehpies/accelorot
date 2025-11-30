747cd6316bb38bb5c0dffe97ffe818d55a104739
Shinzo <131604296+Shinzho@users.noreply.github.com>
2025-11-30 11:34:02 +0800

    combine data file

--- lib/data/providers/repository_providers.dart
+++ lib/data/providers/repository_providers.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../data/repositories/activity_repository.dart';
-import '../../../data/repositories/firestore_activity_repository.dart';
 
 final activityRepositoryProvider = Provider<ActivityRepository>((ref) {
   return FirestoreActivityRepository();
