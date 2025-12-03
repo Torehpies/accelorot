1f1559da8b7b64fca88cb35200c04c3bf3ad0075
Rashly Estremadura <rashlyremorozoestremadura@gmail.com>
2025-12-03 18:24:15 +0800

    AC-283: web_statistics
    add web_statistics
    rename statistics to mobile_statistics

--- lib/frontend/operator/main_navigation.dart
+++ lib/frontend/operator/main_navigation.dart
@@ -1,7 +1,7 @@
 // lib/frontend/screens/main_navigation.dart
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/ui/statistics/statistics_screen.dart';
+import 'package:flutter_application_1/ui/mobile_statistics/statistics_screen.dart';
 import 'dashboard/home_screen.dart';
 import 'package:flutter_application_1/frontend/operator/activity_logs/widgets/activity_logs_navigator.dart';
 import 'profile/profile_screen.dart';
