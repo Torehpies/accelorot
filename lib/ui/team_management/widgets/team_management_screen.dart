dda7e281a724c0304d67fb7cd07e99338743852a
Torehpies <salupadoseiffer@gmail.com>
2025-12-25 23:59:17 +0800

    AC-249 Minor background update

--- lib/ui/team_management/widgets/team_management_screen.dart
+++ lib/ui/team_management/widgets/team_management_screen.dart
@@ -29,7 +29,7 @@ class TeamManagementScreen extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Team Management", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.yellow.shade800,
+        backgroundColor: AppColors.background,
         actions: [
           IconButton(
             onPressed: (teams.isLoading || isSaving)
