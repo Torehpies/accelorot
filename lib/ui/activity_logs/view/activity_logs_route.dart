8bba9160cb9d8b2301b4abc2887ed80f465228dc
Shinzo <131604296+Shinzho@users.noreply.github.com>
2026-01-15 19:03:46 +0800

    Update activity_logs_route.dart

--- lib/ui/activity_logs/view/activity_logs_route.dart
+++ lib/ui/activity_logs/view/activity_logs_route.dart
@@ -1,5 +1,6 @@
 // lib/ui/activity_logs/view/activity_logs_route.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/ui/core/ui/responsive_layout.dart';
 import 'activity_logs_navigator.dart';
 import 'unified_activity_view.dart';
 
@@ -8,15 +9,9 @@ class ActivityLogsRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        final width = MediaQuery.sizeOf(context).width;
-        
-        if (width >= 900) {
-          return const UnifiedActivityView();
-        }
-        return const ActivityLogsNavigator();
-      },
+    return ResponsiveLayout(
+      mobileView: const ActivityLogsNavigator(),
+      desktopView: const UnifiedActivityView(),
     );
   }
 }
