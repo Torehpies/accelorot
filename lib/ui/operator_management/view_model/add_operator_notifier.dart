506865cce3819ebf549b750e925eb109da99e01d
Torehpies <salupadoseiffer@gmail.com>
2026-02-11 17:08:29 +0800

    AC-509: Updated Add Operator to call increment Team Field

--- lib/ui/operator_management/view_model/add_operator_notifier.dart
+++ lib/ui/operator_management/view_model/add_operator_notifier.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_application_1/data/models/app_user.dart';
 import 'package:flutter_application_1/data/providers/auth_providers.dart';
 import 'package:flutter_application_1/data/providers/operator_providers.dart';
+import 'package:flutter_application_1/data/providers/team_providers.dart';
+import 'package:flutter_application_1/data/services/contracts/team_service.dart';
 import 'package:flutter_application_1/data/services/firebase/firebase_operator_service.dart';
 import 'package:flutter_application_1/data/utils/result.dart';
 import 'package:flutter_application_1/utils/roles.dart';
@@ -12,16 +14,18 @@ part 'add_operator_notifier.g.dart';
 
 @riverpod
 class AddOperatorNotifier extends _$AddOperatorNotifier {
-  late final FirebaseOperatorService _service;
+  late final FirebaseOperatorService _operatorService;
+  late final TeamService _teamService;
   @override
   AddOperatorState build() {
-    _service = ref.read(operatorServiceProvider);
+    _operatorService = ref.read(operatorServiceProvider);
+    _teamService = ref.read(teamServiceProvider);
     return const AddOperatorState(isLoading: false);
   }
 
   /// SIPIR - Added operators are email verified
-	/// as upon setting their password means
-	/// they have access to that email
+  /// as upon setting their password means
+  /// they have access to that email
 
   Future<void> addOperator({
     required String email,
@@ -36,7 +40,7 @@ class AddOperatorNotifier extends _$AddOperatorNotifier {
     final teamId = teamUser?.teamId;
     if (teamId == null) return;
 
-    final result = await _service.addUser(
+    final result = await _operatorService.addUser(
       email: email,
       firstname: firstname,
       lastname: lastname,
@@ -46,12 +50,30 @@ class AddOperatorNotifier extends _$AddOperatorNotifier {
       requestTeamId: teamId,
     );
     // Handle success and errors
+
     if (result is Ok<AppUser>) {
-      final appUser = result.value;
-      state = state.copyWith(isLoading: false, operator: appUser);
+      final incrementResult = await _teamService.incrementTeamField(
+        teamId: teamId,
+        field: "activeOperators",
+        amount: 1,
+      );
+
+      if (incrementResult is Ok<String>) {
+        final appUser = result.value;
+        state = state.copyWith(isLoading: false, operator: appUser);
+      } else if (incrementResult is Error<String>) {
+        final error = incrementResult.error;
+        state = state.copyWith(
+          isLoading: false,
+          error: 'Failed to update Team Summary: ${error.toString()}',
+        );
+      }
     } else if (result is Error<AppUser>) {
       final error = result.error;
-      state = state.copyWith(isLoading: false, error: error.toString());
+      state = state.copyWith(
+        isLoading: false,
+        error: 'Failed to Add Operator: ${error.toString()}',
+      );
     }
   }
 }
