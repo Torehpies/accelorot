2b4387b3f3a874c021ee743f5911e0ea92cd486f
Torehpies <salupadoseiffer@gmail.com>
2026-02-17 20:46:53 +0800

    BREAKING CHANGE: Breaks add team Initial update on add team dialog Signed-off-by: Seiffer salupadoseiffer@gmail.com

--- lib/ui/team_management/widgets/add_team_dialog.dart
+++ lib/ui/team_management/widgets/add_team_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/frontend/widgets/custom_text_field.dart';
 import 'package:flutter_application_1/ui/core/themes/app_theme.dart';
 import 'package:flutter_application_1/ui/core/widgets/dialog_shell.dart';
 import 'package:flutter_application_1/ui/team_management/view_model/team_management_notifier.dart';
@@ -15,29 +14,19 @@ class AddTeamDialog extends ConsumerStatefulWidget {
 
 class _AddTeamDialogState extends ConsumerState<AddTeamDialog> {
   final _formKey = GlobalKey<FormState>();
-  final nameController = TextEditingController();
-  final addressController = TextEditingController();
-
-  @override
-  void dispose() {
-    nameController.dispose();
-    addressController.dispose();
-    super.dispose();
-  }
-
-  void _addTeam() {
-    if (_formKey.currentState!.validate()) {
-      ref
-          .read(teamManagementProvider.notifier)
-          .addTeam(nameController.text, addressController.text);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(teamManagementProvider);
     final isSaving = state.isSavingTeams;
     final errorMessage = state.message;
+
+    String _teamName = '';
+    String _houseNumber = '';
+    String _street = '';
+    String _barangay = '';
+    String _city = '';
+    String _region = '';
 
     ref.listen(teamManagementProvider, (previous, next) {
       if (next.isSavingTeams == false &&
@@ -78,45 +67,108 @@ class _AddTeamDialogState extends ConsumerState<AddTeamDialog> {
                     ],
                   ),
                 ),
-              CustomTextField(
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Team',
+                  labelText: 'Team Name',
+                ),
+                validator: (value) => (value == null || value.trim().isEmpty)
+                    ? 'Team Name is required'
+                    : (value.trim().length < 3)
+                    ? 'Team name must be at least 3 characters'
+                    : null,
                 enabled: !isSaving,
-                labelText: "Team Name",
-                prefixIcon: Icons.group,
-                controller: nameController,
-                autoFocus: true,
-                validator: (value) {
-                  final trimmed = value?.trim();
-                  if (trimmed == null || trimmed.isEmpty) {
-                    return "Please enter a team name.";
-                  }
-                  if (trimmed.length < 3) {
-                    return "Team name must be at least 3 characters.";
-                  }
-                  return null;
-                },
                 textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.text,
+                onSaved: (value) => _teamName = value!.trim(),
               ),
               SizedBox(height: 10),
-              CustomTextField(
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: '1111 / Lot 10 Blk 10',
+                  labelText: 'House/Lot/Block',
+                ),
+                validator: (value) => (value == null || value.trim().isEmpty)
+                    ? 'This field is required'
+                    : (value.trim().length < 3)
+                    ? 'This field must be at least 4 characters'
+                    : null,
                 enabled: !isSaving,
-                onFieldSubmitted: (value) => isSaving ? null : _addTeam(),
-                autoFocus: true,
-                labelText: "Address",
-                prefixIcon: Icons.location_on,
-                controller: addressController,
+                textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.streetAddress,
-                validator: (value) {
-                  final trimmed = value?.trim();
-                  if (trimmed == null || trimmed.isEmpty) {
-                    return "Please enter an address.";
-                  }
-                  if (trimmed.length < 20) {
-                    return "Address name must be at least 20 characters.";
-                  }
-                  return null;
-                },
-                textInputAction: TextInputAction.done,
+                onSaved: (value) => _houseNumber = value!.trim(),
               ),
+              SizedBox(height: 10),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Libra St. / Zabarte Road / Luisa Subd.',
+                  labelText: 'Street/Road/Subd.',
+                ),
+                validator: (value) => (value == null || value.trim().isEmpty)
+                    ? 'This field is required'
+                    : (value.trim().length < 4)
+                    ? 'This field must be at least 4 characters'
+                    : null,
+                enabled: !isSaving,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.streetAddress,
+                onSaved: (value) => _street = value!.trim(),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: '178 / 176-E',
+                  labelText: 'Barangay',
+                ),
+                validator: (value) => (value == null || value.trim().isEmpty)
+                    ? 'This field is required'
+                    : (value.trim().length > 5)
+                    ? 'This field has a maximum of 5 characters'
+                    : null,
+                enabled: !isSaving,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.text,
+                onSaved: (value) => _barangay = value!.trim(),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Caloocan City',
+                  labelText: 'City',
+                ),
+                validator: (value) => (value == null || value.trim().isEmpty)
+                    ? 'This field is required'
+                    : (value.trim().length < 4)
+                    ? 'This field must be at least 4 characters'
+                    : null,
+                enabled: !isSaving,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.text,
+                onSaved: (value) => _city = value!.trim(),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Region 1 / NCR',
+                  labelText: 'Region',
+                ),
+                validator: (value) => (value == null || value.trim().isEmpty)
+                    ? 'This field is required'
+                    : (value.trim().length < 3)
+                    ? 'This field must be at least 3 characters'
+                    : null,
+                enabled: !isSaving,
+                textInputAction: TextInputAction.done,
+                keyboardType: TextInputType.text,
+                onSaved: (value) => _region = value!.trim(),
+              ),
+              SizedBox(height: 10),
             ],
           ),
         ),
@@ -139,5 +191,16 @@ class _AddTeamDialogState extends ConsumerState<AddTeamDialog> {
         ),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _addTeam() {
+    if (_formKey.currentState!.validate()) {
+      ref.read(teamManagementProvider.notifier).addTeam("chan", "TODO");
+    }
   }
 }
