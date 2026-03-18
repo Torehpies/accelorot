49bcf40720167865fef58e6c1aff5054fa060084
Jasupaa <129401710+Jasupaa@users.noreply.github.com>
2026-03-18 08:04:12 +0800

    Add auth observer to register FCM token

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/routes/app_router.dart';
 import 'package:flutter_application_1/ui/core/themes/app_theme.dart';
 import 'package:flutter_application_1/data/providers/core_providers.dart';
+import 'package:flutter_application_1/data/providers/auth_observer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
@@ -49,6 +50,9 @@ class _MyAppState extends ConsumerState<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    // Activate auth observer to handle side effects like FCM token registration
+    ref.watch(authObserverProvider);
+    
     final router = ref.watch(routerProvider);
     return MaterialApp.router(
       title: 'Accel-o-Rot',
