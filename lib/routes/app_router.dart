a4beafa42145f13d891fee3852c6270158c6bdb1
Torehpies <salupadoseiffer@gmail.com>
2026-03-21 16:37:37 +0800

    feat(google login): routing changes for new complete profile page

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -44,9 +44,11 @@ import 'package:flutter_application_1/ui/web_landing_page/widgets/privacy_policy
 import 'package:flutter_application_1/frontend/screens/Onboarding/resent_email_sent_screen.dart';
 import 'package:flutter_application_1/ui/splashscreen/views/splash_screen_view.dart';
 import 'package:flutter_application_1/ui/qr_scan/view/qr_scan_screen.dart';
+import 'package:flutter_application_1/ui/social_login/view/complete_profile_screen.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
   final notifier = ref.watch(routerNotifierProvider);
+
 
   return GoRouter(
     refreshListenable: notifier,
@@ -96,7 +98,13 @@ final routerProvider = Provider<GoRouter>((ref) {
         builder: (context, state) => const RegistrationScreen(),
       ),
       GoRoute(
+        path: RoutePath.completeProfile.path,
+        name: RoutePath.completeProfile.name,
+        builder: (context, state) => const CompleteProfileScreen(),
+      ),
+      GoRoute(
         path: RoutePath.forgotPassword.path,
+
         name: RoutePath.forgotPassword.name,
         builder: (context, state) => const ForgotPassScreen(),
       ),
